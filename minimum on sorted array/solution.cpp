66cf356c5a28f60556f839ed80945e738fa023cd
Johnfor93 <jonathanalphabert0504@gmail.com>
2023-07-06 09:53:39 +0700

    Change solution of minimum on rotated sorted array

--- minimum on sorted array/solution.cpp
+++ minimum on sorted array/solution.cpp
@@ -2,29 +2,27 @@
     The idea: https://www.geeksforgeeks.org/search-an-element-in-a-sorted-and-pivoted-array/
 
     Because of this array sorted, then we should find the data with this condition
-    data[i] > data[i+1]
-    where data[i+1] is the smallest data in array because we know that array sorted in ascending order
+    data[left] < data[right]
+    then data[left] is the smallest data in array because we know that array sorted in ascending order
 
     Time Complexity: O(log N)
 */
 
 class Solution {
 public:
-    int pivotSearch(vector<int>&nums, int left, int right)
-    {
-        if(left > right || left == nums.size()-1) return 0;
-
-        int mid = (right+left)/2;
-
-        // Check next data for 
-        if(nums[mid]>nums[mid+1]) return mid+1;
-        if(nums[mid-1]>nums[mid]) return mid;
-
-        if(nums[left] < nums[mid]) return pivotSearch(nums, left, mid-1);
-        return pivotSearch(nums, mid, right);
-    }
     int findMin(vector<int>& nums) {
-        int pivotedIndex = pivotSearch(nums, 0, nums.size()-1);
-        return nums[pivotedIndex];
+        int size = nums.size();
+        int left = 0, right = size-1;
+        while(left < right)
+        {
+            int mid = left + (right - left) / 2;
+
+            // check if sorted, then left is the pivot Index
+            if(nums[left] < nums[right]) return nums[left];
+
+            if(nums[left] <= nums[mid]) left = mid+1;
+            else right = mid;
+        }
+        return nums[left%size];
     }
 };
