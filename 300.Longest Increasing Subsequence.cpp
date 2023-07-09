3e9ee92ae9b27d9a56a614b3db14f830dd91f6e5
Johnfor93 <jonathanalphabert0504@gmail.com>
2023-07-09 22:09:28 +0700

    Longest Increasing Subsequence With Faster Solution Added

--- 300.Longest Increasing Subsequence.cpp
+++ 300.Longest Increasing Subsequence.cpp
@@ -28,6 +28,32 @@ int lengthOfLIS(vector<int>& nums) {
 	return *max_element(dp.begin(), dp.end());
 }
 
+    // Trying O(n log n) solution
+
+    // The approach is every iteration is
+int lengthOfLIS(vector<int>& nums) {
+    int n = nums.size();
+    // It's keep the subSequence element's
+    vector<int> subSequence;
+    subSequence.push_back(nums[0]);
+    for(int i=1; i<n; i++){
+        // Check if nums[i] < subSequence.end() there is new data is new data is highest
+        if(nums[i] > subSequence.back()){
+            subSequence.push_back(nums[i]);
+        }
+        else{
+            // search to replace
+            // Using lower_bound() function with O(log N) Time Complexity
+            // Source: https://www.geeksforgeeks.org/lower_bound-in-cpp/
+            // Iterator thats mean this is a pointer
+            vector<int>::iterator low;
+            low = lower_bound(subSequence.begin(), subSequence.end(), nums[i]);
+            *low = nums[i];
+        }
+    }
+    return subSequence.size();
+}
+
 int main(){
 	vector<int> nums{4, 3, 5, 2, 8 , 9};
 	
